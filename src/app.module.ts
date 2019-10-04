ce2ee7b97cd0a61c79e0310531d2e46c3f0bb78e
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2019-10-04 16:55:05 +0300

    auth module and user repository added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { TasksModule } from './tasks/tasks.module';
 import { typeormConfig } from './config/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeormConfig),
-    TasksModule
+    TasksModule,
+    AuthModule
   ],
 })
 export class AppModule {}
