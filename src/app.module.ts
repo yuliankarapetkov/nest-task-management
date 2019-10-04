20ea9bfdfec043c2ff46609b23b51cfb7c631358
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2019-10-04 14:23:08 +0300

    applied the typeorm config to the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { TasksModule } from './tasks/tasks.module';
+import { typeormConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TypeOrmModule.forRoot(typeormConfig),
+    TasksModule
+  ],
 })
 export class AppModule {}
